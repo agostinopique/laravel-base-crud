227ccf3ac8586bc4d114d1589308238528123d95
agostinopique <ago.pique@yahoo.it>
2022-06-29 16:25:10 +0200

    First commit;

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('comic.index') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Our Comics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">AAA</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
