d9d2c04905cb809bcfb16cc8196f1139aa40fe7b
agostinopique <ago.pique@yahoo.it>
2022-07-04 16:45:05 +0200

    Aggiunta classe dinamica in base a old();

--- resources/views/view-crud/create.blade.php
+++ resources/views/view-crud/create.blade.php
@@ -18,21 +18,21 @@
             @csrf
             <div class="mb-3">
                 <label for="comicTitle" class="form-label">Comic title</label>
-                <input type="text" class="form-control" id="comicTitle" name="title" value="{{old('title')}}">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="comicTitle" name="title" value="{{old('title')}}">
                 @error('title')
                     <p class="alert alert-danger mt-1">{{$message}}</p>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="comicType" class="form-label">Type</label>
-                <input type="text" class="form-control" id="comicType" name="type" value="{{old('type')}}">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" id="comicType" name="type" value="{{old('type')}}">
                 @error('type')
                     <p class="alert alert-danger mt-1">{{$message}}</p>
                 @enderror
             </div>
             <div class="mb-3">
                 <label for="comicImage" class="form-label">Comic Cover Image</label>
-                <input type="text" class="form-control" id="comicImage" name="image" value="{{old('image')}}">
+                <input type="text" class="form-control @error('image') is-invalid @enderror" id="comicImage" name="image" value="{{old('image')}}">
                 @error('image')
                     <p class="alert alert-danger mt-1">{{$message}}</p>
                 @enderror
